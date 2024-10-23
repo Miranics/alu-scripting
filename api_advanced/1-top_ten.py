f42a9c779ff99dde86b7d68e5d172358c98e5232
Miracle <002nasya@gmail.com>
2024-10-23 16:53:33 +0200

    black formatter

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,19 +7,13 @@ def top_ten(subreddit):
     """Return number of subscribers if @subreddit is valid subreddit.
     if not return 0."""
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    headers = {"User-Agent": "MyAPI/0.0.1"}
     subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
     response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
         json_data = response.json()
         for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+            print(json_data.get("data").get("children")[i].get("data").get("title"))
     else:
         print(None)
-        
