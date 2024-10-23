007bcd064a66f6cfa6d98089d6db93e90d1c38d5
Miracle <002nasya@gmail.com>
2024-10-23 17:37:47 +0200

    return loop fixed

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,13 +4,16 @@ import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed in a subreddit"""
+    """Prints the titles of the first 10 hot posts listed in a subreddit"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code != 200:
-        print("OK")
+        print(None)  # Print None instead of "OK" for an invalid subreddit
         return
-    posts = response.json()["data"]["children"]
+
+    posts = response.json().get("data", {}).get("children", [])
+    
     for post in posts:
-        print(post["data"]["title"])
+        print(post["data"].get("title"))  # Print the titles of the posts
