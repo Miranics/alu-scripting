fbce621956cfa5eebb1d31b4a9f8349338b2b832
Miracle <002nasya@gmail.com>
2024-10-25 16:03:28 +0200

    url fixed

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""
+This module provides a function to print the titles of the first 10 hot posts 
+listed for a given subreddit.
+"""
+
 import requests
 
-
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        None
+    """
+    URL = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    HEADERS = {"User-Agent": "CustomUserAgent/1.0"}
+
+    try:
+        response = requests.get(URL, headers=HEADERS, allow_redirects=False)
         
+        # Check if the request was successful
+        if response.status_code != 200:
+            print("None")
+            return
+        
+        # Parse JSON response and print titles
+        data = response.json().get("data", {}).get("children", [])
+        if not data:
+            print("None")
+        else:
+            for post in data:
+                print(post.get("data", {}).get("title"))
+
+    except Exception:
+        print("None")
