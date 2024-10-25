1731cf906d692b20d8e7bc83c640af39db7959f7
Miracle <002nasya@gmail.com>
2024-10-25 18:30:08 +0200

    refotmatted

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,18 +4,14 @@ import requests
 
 
 def top_ten(subreddit):
-    """"Doc"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
-        .format(subreddit)
+    """ "Doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    res = requests.get(url,
-                       headers={
-                           'User-Agent': 'Mozilla/5.0'})
+    res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
 
     if res.status_code != 200:
         print(None)
     else:
         json_response = res.json()
-        posts = json_response.get('data').get('children')
-        [print(post.get('data').get('title')) for post in posts]
-        
+        posts = json_response.get("data").get("children")
+        [print(post.get("data").get("title")) for post in posts]
