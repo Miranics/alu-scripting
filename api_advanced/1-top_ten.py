7a53c3b9d65d5250fde9e8a6dd06021c1432160b
Miracle <002nasya@gmail.com>
2024-10-25 19:10:50 +0200

    output fixed

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""""Doc"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """ "Doc"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-
-    if res.status_code != 200:
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
         print(None)
-    else:
-        json_response = res.json()
-        posts = json_response.get("data").get("children")
-        [print(post.get("data").get("title")) for post in posts]
