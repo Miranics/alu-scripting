e40723a7b51fcbbc71e3ede49209bb0d4624d8b0
Miracle <002nasya@gmail.com>
2024-10-26 16:13:57 +0200

    url fixed

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-'''
-Defines function that prints the top ten posts of a subreddit
-'''
+""""Doc"""
 import requests
 
 
 def top_ten(subreddit):
-    '''Prints the top ten posts of a subreddit
+    """"Doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
 
-    Return:
-        None -  if the subreddit is invalid
-    '''
-    if subreddit is None or not isinstance(subreddit, str):
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
+
+    if res.status_code != 200:
         print(None)
-    endpoint = 'https://www.reddit.com'
-    headers = {'user-agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/Miracle)'}
-    params = {'limit': 10}
-    info = requests.get('{}/r/{}/hot.json'.format(endpoint, subreddit),
-                        allow_redirects=False,
-                        headers=headers,
-                        params=params)
-    if info.status_code == 200:
-        json_info = info.json()
-        for post in json_info.get('data').get('children'):
-            print(post.get('data').get('title'))
     else:
-        print(None)
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
+        
