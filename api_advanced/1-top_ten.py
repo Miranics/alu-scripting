32212dfe161ecb3445f448f1dab0c66f281a2afd
Miracle <002nasya@gmail.com>
2024-10-26 21:51:22 +0200

    ..

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """A module to query the Reddit API for hot posts."""
-
 import requests
 import sys
-
-
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts listed in a subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
