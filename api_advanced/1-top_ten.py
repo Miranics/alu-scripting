2cc441bb3a080a10022c0cc051b8f1cb9c79b836
Miracle <002nasya@gmail.com>
2024-10-26 19:38:39 +0200

    .

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -12,18 +12,18 @@ def top_ten(subreddit):
         url, headers={"User-Agent": "Mozilla/5.0"}, allow_redirects=False
     )
 
-    # Check if the request was successful
-    if res.status_code != 200:
-        print("OK", end="")
-        return
-
     # Parse the JSON response
-    json_response = res.json()
-    posts = json_response.get("data", {}).get("children", [])
+    if res.status_code == 200:
+        json_response = res.json()
+        posts = json_response.get("data", {}).get("children", [])
 
-    # Print the titles of the first 10 hot posts
-    for post in posts:
-        print(post.get("data", {}).get("title"))
+        # Print the titles of the first 10 hot posts
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+
+    # Print OK only once
+    print("OK", end="")  # Print OK without a newline
+
 
 # Test the function with the learnpython subreddit
 top_ten("learnpython")
