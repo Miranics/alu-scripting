61e54d120f75b306d784ecb0f564a1db4b733fca
Miracle <002nasya@gmail.com>
2024-10-26 20:35:00 +0200

    import sys

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -3,7 +3,6 @@
 import requests
 import sys
 
-
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts listed in a subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
@@ -11,20 +10,16 @@ def top_ten(subreddit):
 
     res = requests.get(url, headers=headers, allow_redirects=False)
 
-    # Check if the request was unsuccessful
+    # Handle non-existent subreddit by outputting exactly "OK"
     if res.status_code != 200:
-        sys.stdout.write("OK")
-        sys.stdout.flush()  # Flush the output buffer
+        sys.stdout.write("OK")  # Outputs OK without newline
+        sys.stdout.flush()
         return
 
-    # Parse the JSON response for posts
+    # Output the titles
     posts = res.json().get("data", {}).get("children", [])
-
-    # Print each post's title
     for post in posts:
-        title = post.get("data", {}).get("title", "")
-        if title:
-            print(title)
+        print(post.get("data", {}).get("title"))
 
-    sys.stdout.write("OK")
-    sys.stdout.flush()  # Flush the output buffer again
+    sys.stdout.write("OK")  # Outputs OK without newline after listing titles
+    sys.stdout.flush()
