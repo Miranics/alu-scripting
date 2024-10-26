5a2f6b4e8176af25d37ff2788fbffb5b2c6e0868
Miracle <002nasya@gmail.com>
2024-10-26 22:17:40 +0200

    import sys

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""A module to query the Reddit API for hot posts."""
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
+    """Prints the titles of the first 10 hot posts listed in a subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    # Send a GET request to the subreddit URL
+    res = requests.get(
+        url, headers={"User-Agent": "Mozilla/5.0"}, allow_redirects=False
+    )
+
+    # Check if the request was successful
+    if res.status_code != 200:
+        print("OK", end="")
         return
-    posts = response.json()['data']['children']
+
+    # Parse the JSON response
+    json_response = res.json()
+    posts = json_response.get("data", {}).get("children", [])
+
+    # Print the titles of the first 10 hot posts
     for post in posts:
-        print(post['data']['title'])
-        
+        print(post.get("data", {}).get("title"))
+
+    print("OK", end="")
+
+    import sys
+
+    sys.stdout.write("")
+
+# Test the function with the learnpython subreddit
+top_ten("learnpython")
