94e2f0bed9bf5ab5b6567110dd5eecae4cb0e258
Miracle <002nasya@gmail.com>
2024-10-26 21:45:57 +0200

    proper module doc

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""
-A module to query the Reddit API for hot posts.
+"""A module to query the Reddit API for hot posts.
 
 This module contains the function `top_ten`, which retrieves and prints
 the titles of the first 10 hot posts from a specified subreddit. If the
 subreddit is invalid, it will print "OK".
 
 Usage:
-    python3 1-main.py <subreddit_name>
-"""
+    python3 1-main.py <subreddit_name>"""
 
 import requests
 import sys
 
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed in a subreddit.
+    """Prints the titles of the first 10 hot posts listed in a subreddit.
 
     Parameters:
     subreddit (str): The name of the subreddit to query.
 
     If the subreddit does not exist or cannot be accessed, it prints "OK".
-    Otherwise, it prints the titles of the first 10 hot posts and "OK".
-    """
+    Otherwise, it prints the titles of the first 10 hot posts and "OK"."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
 
