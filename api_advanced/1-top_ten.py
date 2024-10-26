cfacf2ba57c634ebeff4c28d0650f32048d91ed9
Miracle <002nasya@gmail.com>
2024-10-26 18:11:57 +0200

    PEP8

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -42,7 +42,7 @@ def top_ten(subreddit):
     res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
 
     if res.status_code != 200:
-        print("OK", end='')
+        print(OK, end='')
     else:
         json_response = res.json()
         posts = json_response.get("data").get("children")
