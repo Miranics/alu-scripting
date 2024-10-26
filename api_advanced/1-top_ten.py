3ee44caec4973630b7623b0d4ec45d02dd45bec7
Miracle <002nasya@gmail.com>
2024-10-26 20:42:48 +0200

    sys import

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -3,6 +3,7 @@
 import requests
 import sys
 
+
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts listed in a subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
