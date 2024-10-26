21370462c2b53231f8211a2f0b45bb8f46435ea7
Miracle <002nasya@gmail.com>
2024-10-26 22:01:36 +0200

    ..

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """A module to query the Reddit API for hot posts."""
 import requests
-
+import sys
 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts listed in a subreddit."""
-     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "Mozilla/5.0"}
 
     res = requests.get(url, headers=headers, allow_redirects=False)
